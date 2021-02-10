28000583e4521be0fd3ffd3064604a10bcb5f15a
Priyobrato Das <priyodas12@gmail.com>
2021-02-10 20:00:16 +0530

    test case added...

---
+++ src/main/java/io/math/utility/Calculation.java
@@ -0,0 +1,7 @@
+package io.math.utility;
+
+public class Calculation {
+    public int addNumbers(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/io/math/utility/CalculationTest.java
@@ -0,0 +1,25 @@
+package io.math.utility;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculationTest {
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("after each");
+    }
+
+    @Test
+    void addNumbers() {
+        System.out.println("this test should run...");
+    }
+}
